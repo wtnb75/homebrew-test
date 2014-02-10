2e1d077b0e7e19d8a56cfec7b387ba7aea0b7100
Takashi WATANABE <wtnb75@nifty.com>
2014-02-10 22:58:30 +0900

    gendiff

---
+++ Formula/gendiff.rb
@@ -0,0 +1,6 @@
+require "formula"
+
+class Gendiff < ScriptFileFormula
+  url "http://rpm.org/gitweb?p=rpm.git;a=blob_plain;f=scripts/gendiff;hb=HEAD"
+  sha1 "7c4600a709e965faa8b87d1e779cfe217f5b6e4e"
+end
