3353b35575189e4459f798d530d26766f71367d5
Takashi WATANABE <wtnb-t@iij.ad.jp>
2014-02-12 10:57:44 +0900

    gendiff

--- Formula/gendiff.rb
+++ Formula/gendiff.rb
@@ -1,6 +1,7 @@
 require "formula"
 
 class Gendiff < ScriptFileFormula
-  url "http://rpm.org/gitweb?p=rpm.git;a=blob_plain;f=scripts/gendiff;hb=HEAD"
+  url "http://rpm.org/gitweb?p=rpm.git;a=blob_plain;hb=refs/heads/rpm-4.11.x;f=scripts/gendiff", :using => :curl
   sha1 "7c4600a709e965faa8b87d1e779cfe217f5b6e4e"
+  version "4.11"
 end
