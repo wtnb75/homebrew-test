ad245bf0dc67938de043e8d9c842c8c7d9cb9105
Takashi WATANABE <wtnb-t@iij.ad.jp>
2014-03-12 16:18:35 +0900

    python3

---
+++ Casks/python3.rb
@@ -0,0 +1,7 @@
+class Python3 < Cask
+  url 'http://www.python.org/ftp/python/3.3.5/python-3.3.5-macosx10.6.dmg'
+  homepage 'http://www.python.org/'
+  version '3.3.5'
+  sha256 '7e59f823f82da5ec7e2af4449a5e33c09f5b755a8acd9cec98371da8c2b2b52b'
+  install 'Python.mpkg'
+end
