e6e578bee04cdee86bf50017307976d3b47bb7a7
Takashi WATANABE <wtnb75@nifty.com>
2014-03-13 23:13:56 +0900

    ldd,rakutenkobo

---
+++ Casks/rakutenkobo.rb
@@ -0,0 +1,7 @@
+class Rakutenkobo < Cask
+  url 'http://download.kobobooks.com/desktop/RakutenBooks/KoboSetup.dmg'
+  homepage 'http://kobo.rakuten.co.jp/application/'
+  version '3.5.2'
+  sha256 'e0df92ca3707f9ceffe2bae26667861cdc219d2ceac6e0fd0d71bdfc32dc08dc'
+  link 'Kobo.app'
+end
