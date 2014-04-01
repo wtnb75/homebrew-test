7de88eec268c03f4fb525eee017efb5cdff61b17
Takashi WATANABE <wtnb75@nifty.com>
2014-04-01 23:46:22 +0900

    bluestacks(not worked)

---
+++ Casks/bluestacks.rb
@@ -0,0 +1,7 @@
+class Bluestacks < Cask
+  url 'http://cdn.bluestacks.com/public/mac-beta/BlueStacks_AppPlayer-Beta.dmg'
+  homepage 'http://www.bluestacks.com/'
+  version 'Beta'
+  sha256 'cfc331273dcfcaa3a4ae9599285d352b0871dd16e4b701b7d8594e28c9a07826'
+  link 'BlueStacks.app'
+end
