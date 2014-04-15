84301cb49a3a032d65de766291c7b39763d056be
Takashi WATANABE <wtnb75@nifty.com>
2014-04-16 00:02:52 +0900

    libreofficeja

--- Casks/libreofficeja.rb
+++ Casks/libreofficeja.rb
@@ -1,8 +1,8 @@
 class Libreofficeja < Cask
-  url 'http://download.documentfoundation.org/libreoffice/stable/4.2.1/mac/x86/LibreOffice_4.2.1_MacOS_x86_langpack_ja.dmg'
+  url 'http://download.documentfoundation.org/libreoffice/stable/4.2.3/mac/x86/LibreOffice_4.2.3_MacOS_x86_langpack_ja.dmg'
   homepage 'http://ja.libreoffice.org/'
-  version '4.2.1'
-  sha256 '1334076aad80572b35af1cc730575ed8f48fdb794d50bb3458ce6aa4cfeb8dbe'
+  version '4.2.3'
+  sha256 'ac2ca1f14cf15effee3aa3d795eacb26dd39b3f0a5a64b959f27aaec34f4b62b'
   caveats do
     manual_installer 'LibreOffice_ja_Language_Pack.app'
   end
