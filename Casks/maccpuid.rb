5d66be43ff4a11e492d44e69e16396b3bc29dd75
Takashi WATANABE <wtnb-t@iij.ad.jp>
2014-05-20 13:23:36 +0900

    maccpuid

---
+++ Casks/maccpuid.rb
@@ -0,0 +1,7 @@
+class Maccpuid < Cask
+  url 'https://software.intel.com/sites/default/files/article/138357/maccpuid-2.1.1.dmg'
+  homepage 'https://software.intel.com/en-us/articles/download-maccpuid'
+  version '2.1.1'
+  sha256 '132e4a19bf7946e7b9f0cebce7c0783e70f1e760b4baa4c365ec2019bf0ddcac'
+  link 'MacCPUID.app'
+end
