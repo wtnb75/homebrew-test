0563013dc8abc030b5e599608be8bba200deb03a
Takashi WATANABE <wtnb75@nifty.com>
2014-12-30 20:31:27 +0900

    dev

--- Formula/s3fs-osxfuse.rb
+++ Formula/s3fs-osxfuse.rb
@@ -7,15 +7,14 @@ class S3fsOsxfuse < Formula
     url "https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.78.tar.gz"
     sha1 "613b448d84451400d3ee14aa9104ba6d9e90bd0b"
     patch do
-      url "https://gist.githubusercontent.com/wtnb75/160909c2b1a37c6d9d0d/raw/496bca6efde64df2482d0fd5ae59580bc13b132d/s3fs-fuse-1.78-osxfuse.patch"
-      sha1 "0a0326f4b76ece1c9979d6dd5fe0d1a3373cf1f5"
+      url "https://gist.githubusercontent.com/wtnb75/160909c2b1a37c6d9d0d/raw/5effbb50e1e58f60581c4b94e02ba98a9a86b407/s3fs-fuse-1.78-osxfuse.patch"
+      sha1 "0857abb60e8fde067192bd8d41596fba9ed47fd5"
     end
   end
 
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "gnutls"
   depends_on "nettle"
   depends_on "libgcrypt"
 
@@ -25,11 +24,14 @@ class S3fsOsxfuse < Formula
 
   def install
     system "./autogen.sh"
-    system "./configure", "--disable-dependency-tracking", "--with-gnutls", "--prefix=#{prefix}"
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make", "install"
   end
 
   def caveats; <<-EOS.undent
+    brew cask install osxfuse
+    cp /usr/local/lib/pkgconfig/osxfuse.pc /usr/local/Library/ENV/pkgconfig/10.10
+
     Be aware that s3fs has some caveats concerning S3 "directories"
     that have been created by other tools. See the following issue for
     details:
