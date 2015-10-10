4356067bdd8e3080c64a82fe29a20413b0cfd289
Takashi WATANABE <wtnb75@gmail.com>
2015-10-10 20:59:20 +0900

    jhd update

--- Formula/jhd.rb
+++ Formula/jhd.rb
@@ -1,7 +1,8 @@
 require "formula"
 
 class Jhd < Formula
-  url "http://ftp.freebsd.org/pub/FreeBSD/ports/distfiles/jhd-2.3.tar.gz"
+  #url "http://ftp.freebsd.org/pub/FreeBSD/ports/distfiles/jhd-2.3.tar.gz"
+  url "http://pub.foc.jp/archives/slackware-alteration/sources/jhd-2.3.tar.gz"
   sha1 "6df8f56185b5a079eb624cc6e16eabc767e94d47"
 
   def patches
@@ -17,16 +18,22 @@ class Jhd < Formula
   end
 end
 __END__
---- Makefile.orig	2014-02-10 18:59:33.000000000 +0900
-+++ Makefile	2014-02-10 18:59:44.000000000 +0900
+--- Makefile.orig	2015-10-03 00:58:40.000000000 +0900
++++ Makefile	2015-10-03 00:59:13.000000000 +0900
 @@ -1,4 +1,4 @@
 -CFLAGS        = -O
 +CFLAGS        = -O -Wno-implicit-function-declaration -Wno-return-type -Wno-implicit-int
  
  DEST	      = ${HOME}/bin
  
---- jhd.c.orig	1992-06-03 09:28:44.000000000 +0900
-+++ jhd.c	2014-02-10 18:54:30.000000000 +0900
+@@ -47,5 +47,3 @@
+ 
+ $(DEST)/$(PROGRAM): $(SRCS) $(LIBS) $(HDRS) $(EXTHDRS)
+ 		@make -f $(MAKEFILE) DEST=$(DEST) install
+-###
+-jhd.o: /usr/include/stdio.h /usr/include/ctype.h
+--- jhd.c.orig	2015-10-03 00:58:45.000000000 +0900
++++ jhd.c	2015-10-03 00:59:28.000000000 +0900
 @@ -24,6 +24,8 @@
  #define  EUC     1        /*  if set, kanji code is EUC (Shift-JIS default)  */
  
@@ -36,4 +43,3 @@ __END__
  #include  <ctype.h>
  
  #define  BSIZE      16384       /*  input buffer size */
-
