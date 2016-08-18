31543d9a0169b4e159c982c1d88655992070d757
Takashi WATANABE <wtnb75@gmail.com>
2016-08-18 21:30:40 +0900

    primeseat

---
+++ Casks/primeseat.rb
@@ -0,0 +1,8 @@
+cask 'primeseat' do
+  url 'http://dl.primeseat.net/player/PrimeSeat.dmg'
+  homepage 'http://primeseat.net'
+  version '1.4.0'
+  name 'PrimeSeat'
+  sha256 '68ccceea363e8f253d15c352bb547ddac3685fbba6823bd1c4d7d84414ec3057'
+  pkg 'PrimeSeat.pkg'
+end
