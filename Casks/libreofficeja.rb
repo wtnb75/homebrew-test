3226cf0f905952e2b03f956a56e68a475a698948
Takashi WATANABE <wtnb-t@iij.ad.jp>
2017-03-28 10:05:55 +0900

    caveats

--- Casks/libreofficeja.rb
+++ Casks/libreofficeja.rb
@@ -4,6 +4,6 @@ cask 'libreofficeja' do
   url "http://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64_langpack_ja.dmg"
   homepage 'http://ja.libreoffice.org/'
   caveats do
-    puts 'open LibreOffice_ja_Language_Pack.app'
+    puts 'open "/usr/local/Caskroom/libreofficeja/5.3.1/LibreOffice Language Pack.app"'
   end
 end
