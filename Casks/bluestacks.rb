a252f6195a05bd7c25020ff1f1433581918f4942
Takashi WATANABE <wtnb-t@iij.ad.jp>
2018-08-03 10:32:14 +0900

    remove old

--- Casks/bluestacks.rb
+++
@@ -1,7 +0,0 @@
-cask 'bluestacks' do
-  url 'http://cdn.bluestacks.com/public/mac-beta/BlueStacks_AppPlayer-Beta.dmg'
-  homepage 'http://www.bluestacks.com/'
-  version 'Beta'
-  sha256 'cfc331273dcfcaa3a4ae9599285d352b0871dd16e4b701b7d8594e28c9a07826'
-  link 'BlueStacks.app'
-end
--- Casks/python3.rb
+++
@@ -1,7 +0,0 @@
-cask "python3" do
-  url 'http://www.python.org/ftp/python/3.3.5/python-3.3.5-macosx10.6.dmg'
-  homepage 'http://www.python.org/'
-  version '3.3.5'
-  sha256 '7e59f823f82da5ec7e2af4449a5e33c09f5b755a8acd9cec98371da8c2b2b52b'
-  install 'Python.mpkg'
-end
