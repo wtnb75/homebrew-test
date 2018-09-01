3a89d646ccd68ddd860ac7eb207243e9666faa24
Takashi WATANABE <wtnb75@gmail.com>
2018-09-01 21:01:51 +0900

    checksum

--- Casks/openscad-snapshot.rb
+++ Casks/openscad-snapshot.rb
@@ -1,6 +1,6 @@
 cask 'openscad-snapshot' do
   version '2018.03.17'
-  sha256 'd4d361c673683cda3195d4c84696020023c56c4db46d4a5efff0ab5d4922d55f'
+  sha256 '58330d5118ec921b3ed930adfb35e90fdaa30afc25930f85cc102fdf255112aa'
 
   url "http://files.openscad.org/snapshots/OpenSCAD-#{version}.dmg"
   name 'OpenSCAD'
