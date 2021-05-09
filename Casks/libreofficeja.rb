3b870c143510940b9d36d97332801175905fbda0
Watanabe Takashi <wtnb75@gmail.com>
2021-05-09 23:15:18 +0900

    libreofficeja

--- Casks/libreofficeja.rb
+++ Casks/libreofficeja.rb
@@ -1,6 +1,6 @@
 cask 'libreofficeja' do
-  version '7.1.2'
-  sha256 'fd30ed372dbaaf84b4f8870d2cadefb4f2696a0e5ffef9729105356ffea554fb'
+  version '7.1.3'
+  sha256 'e69f7422a8c0d8719b0c6f9121a5a18603c5adc7ee287c12ab3cd4c8e3cf6fb0'
   url "http://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64_langpack_ja.dmg"
   homepage 'http://ja.libreoffice.org/'
   caveats do
